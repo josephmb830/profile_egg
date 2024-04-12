2df2fca2676ab4ff81db05d9878e4f12426473a5
josephmb830 <josephmoran@duck.com>
2024-04-11 22:40:27 -0500

    pruebas js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 console.log("conexión exitosa")
+/* seleccionar una etiqueta del HTML */
+const selector = document.getElementById("mensaje");
+/* escribir dentro de esa etiqueta con JS */
+selector.innerHTML = "CONEXIÓN EXITOSA ENTRE HTML Y JS";
+
+// const entrada = prompt("¿Qué producto desea comprar?");
+// console.log(entrada);
+// alert(entrada);
+
+const nombre = prompt("¿como te llamas?");
+const mensaje = "Bienvenido " + nombre;
+console.log(mensaje);
+alert(mensaje);
